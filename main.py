a5f9342baaecb36b7501527d37bf2778b7452d01
adiii257 <120394982+Adiii93@users.noreply.github.com>
2024-06-12 18:51:41 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+#masukkan nilai jari - jari
+r = float(input("Masukkan nilai r(jari - jari lingkaran) : "))
+
+#rumus luas lingkaran
+luas = math.pi * r * r
+
+#cetak hasil dari luas lingkaran
+print("luas lingkarannya adalah", luas)
